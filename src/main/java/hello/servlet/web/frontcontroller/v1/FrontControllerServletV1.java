2d9493a23ee1ea5823c2c1aee70161b56f2ce4be
f1v3-dev <f1v3_dev@naver.com>
2023-04-15 22:57:15 +0900

    [프론트 컨트롤러 도입 - V1]

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -0,0 +1,53 @@
+package hello.servlet.web.frontcontroller.v1;
+
+import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * urlPatern에서 *은 해당 하위 url은 모두 이것을 호출한다는 의미
+ */
+@WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
+public class FrontControllerServletV1 extends HttpServlet {
+
+    /**
+     * Key : 매핑된 URL
+     * Value : 호출될 컨트롤러
+     * Map<String, ControllerV1>
+     */
+    private Map<String, ControllerV1> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV1() {
+        controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV1.service");
+
+        // front-controller/v1/members
+        String requestURI = request.getRequestURI();
+
+
+        // ControllerV1 controller = new MemberListControllerV1();
+        ControllerV1 controller = controllerMap.get(requestURI);
+        if (controller == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // @Override 되어서 매핑 정보에 따라 호출
+        controller.process(request, response);
+    }
+}
